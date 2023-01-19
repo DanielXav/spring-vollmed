4ebffe2d354fda944fe1e70da46926850f8b7562
Daniel Xavier <dxavier194@gmail.com>
2023-01-19 16:00:26 -0300

    first commit

---
+++ src/main/java/daniel/xavier/api/controller/HelloController.java
@@ -0,0 +1,15 @@
+package daniel.xavier.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @GetMapping
+    public String helloWord(){
+        return "Hello galerinha";
+    }
+}
