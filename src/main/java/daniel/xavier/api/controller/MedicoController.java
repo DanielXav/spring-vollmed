eefa6f516bc354906b8b424006b62dfd4eef4013
Daniel Xavier <dxavier194@gmail.com>
2023-02-16 14:49:50 -0300

    Pagination Medicos

--- src/main/java/daniel/xavier/api/controller/MedicoController.java
+++ src/main/java/daniel/xavier/api/controller/MedicoController.java
@@ -2,15 +2,18 @@ package daniel.xavier.api.controller;
 
 import daniel.xavier.api.endereco.Endereco;
 import daniel.xavier.api.medico.DadosCadastraMedico;
+import daniel.xavier.api.medico.DadosListagemMedico;
 import daniel.xavier.api.medico.Medico;
 import daniel.xavier.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/medicos")
@@ -23,5 +26,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastraMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
